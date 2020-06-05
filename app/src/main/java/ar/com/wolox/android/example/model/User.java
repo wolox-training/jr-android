ac40b49287ef2d1cf730ffc5c9256918a895a568
Jimenarosello <jimena.rosello@gmail.com>
2020-06-05 18:20:10 -0300

    Remove unnecessary getter

--- app/src/main/java/ar/com/wolox/android/example/model/User.java
+++ app/src/main/java/ar/com/wolox/android/example/model/User.java
@@ -4,10 +4,6 @@ public class User {
     private String email;
     private String password;
 
-    public String getEmail() {
-        return email;
-    }
-
     public String getPassword() {
         return password;
     }
