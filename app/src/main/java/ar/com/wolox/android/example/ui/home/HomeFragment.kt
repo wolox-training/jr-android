5b57a3b204309f95a02851674b3c4a6915b86e85
Jimenarosello <jimena.rosello@gmail.com>
2020-06-16 18:37:31 -0300

    Add news layout

--- app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
@@ -1,12 +1,26 @@
 package ar.com.wolox.android.example.ui.home
 
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ar.com.wolox.android.R
+import ar.com.wolox.android.example.model.NewsArticle
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment private constructor() : WolmoFragment<HomePresenter>(), HomeView {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun init() {
+        viewManager = LinearLayoutManager(activity)
+        viewAdapter = NewsListAdapter(listOf(NewsArticle("test", "test", "test", false)), activity!!)
+
+        recyclerView = vListNews.apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
 
     override fun layout() = R.layout.fragment_home
