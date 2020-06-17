c170561f32037233d6a3b39bb9ddb59a03045235
Jimenarosello <jimena.rosello@gmail.com>
2020-06-16 21:05:17 -0300

    Remove unnecessary question marks

--- app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
@@ -2,7 +2,6 @@ package ar.com.wolox.android.example.ui.home
 
 import ar.com.wolox.android.R
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
-import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment private constructor() : WolmoFragment<HomePresenter>(), HomeView {
 
